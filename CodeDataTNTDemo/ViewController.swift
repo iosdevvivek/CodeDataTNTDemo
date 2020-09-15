06117ea73fdde9690d59c3b685816a7d9215399d
Vivek <vivek.p@rxprism.com>
2020-09-16 02:11:52 +0530

    Initial Commit

---
+++ CodeDataTNTDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeDataTNTDemo
+//
+//  Created by Vivek on 9/16/20.
+//  Copyright © 2020 Vivek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
